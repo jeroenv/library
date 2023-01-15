9abda7ea35089e7496a39ad0e56f6ad6a375de4d
Jeroen Verhoest <jeroen_verhoest@hotmail.com>
2023-01-15 22:25:21 +0100

    Add Boolean util

---
+++ src/core/util/Boolean.ts
@@ -0,0 +1,13 @@
+export default class Boolean {
+  /**
+   * It takes one parameter value of type string which contains the value which is to be converted to boolean.
+   *
+   * It returns a primitive boolean value. It returns the true if the given value is equals “true” ignoring cases.
+   * Else it returns false.
+   * @param value
+   * @returns
+   */
+  public static parseBoolean(value: string): boolean {
+    return value.toLowerCase() === 'true';
+  }
+}
